82a1209abceb853b0942bb838eb579c22a637516
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-03-07 12:57:30 -0700

    Update __args__.py

--- source/__args__.py
+++ source/__args__.py
@@ -7,8 +7,8 @@ class args:
 
     def init_args(self):
         self.parser.add_argument("-o", "--outfile", action="store", default=f"bytesync_{time.time_ns()}.txt")
-        self.parser.add_argument("-l", "--listen")
-        self.parser.add_argument("-t", "--transmit")
+        self.parser.add_argument("-l", "--listen", action="store_true")
+        self.parser.add_argument("-t", "--transmit", action="store_true")
         self.parser.add_argument("-p", "--port", action="store", default="20")
         self.parser.add_argument("-H", "--host", action="store", default="localhost")
         self.parser.add_argument("-f", "--file", action="store")
